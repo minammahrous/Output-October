621aa2c98a536d100f709abf295e4481c3f482db
minammahrous <126823445+minammahrous@users.noreply.github.com>
2025-03-08 23:07:51 +0200

    Update shift_output_form.py

--- pages/shift_output_form.py
+++ pages/shift_output_form.py
@@ -347,11 +347,11 @@ else:
     availability = total_production_time / standard_shift_time if standard_shift_time != 0 else 0
 
 efficiencies = []
-                for product, batch_list in st.session_state.product_batches.items():
-                    for batch in batch_list:
-                        standard_rate = get_standard_rate(product, selected_machine)
-                        efficiency = (batch["quantity"] / (batch["time_consumed"] * standard_rate)) if (standard_rate != 0 and batch["time_consumed"] != 0) else 0
-                        efficiencies.append(efficiency)
+for product, batch_list in st.session_state.product_batches.items():
+    for batch in batch_list:
+        standard_rate = get_standard_rate(product, selected_machine)
+        efficiency = (batch["quantity"] / (batch["time_consumed"] * standard_rate)) if (standard_rate != 0 and batch["time_consumed"] != 0) else 0
+        efficiencies.append(efficiency)
 
 average_efficiency = sum(efficiencies) / len(efficiencies) if efficiencies else 0
 
