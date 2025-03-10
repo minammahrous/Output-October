ea5b1c912978363f1817ec9a98e27d9f65cdb988
minammahrous <126823445+minammahrous@users.noreply.github.com>
2025-03-10 19:49:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ st.write("Use the sidebar to navigate.")
 # Add navigation to pages
 st.page_link("pages/shift_output_form.py", label="Shift Output Form")
 st.page_link("pages/reports_dashboard.py", label="Reports Dashboard")
+st.page_link("pages/master_data.py", label="Master data control")
