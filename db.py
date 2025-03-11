e876bff4437e1796d0534755c0218fe62873cb30
minammahrous <126823445+minammahrous@users.noreply.github.com>
2025-03-11 12:39:47 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from sqlalchemy import create_engine, text
+# Database connection
 
 def get_db_connection():
     """Return a database connection based on the selected branch."""
-    base_url = st.secrets["database"]["url"]  # Read from Streamlit secrets
+    base_url = postgresql://neondb_owner:npg_QyWNO1qFf4do@ep-quiet-wave-a8pgbkwd-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
     branch = st.session_state.get("branch", "main")  # Default to 'main' if not set
     
     # Modify DB URL to use the correct branch
