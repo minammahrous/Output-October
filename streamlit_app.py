c28bbb859a71d975ab59f6e4df6a401d4cdbcdfd
minammahrous <126823445+minammahrous@users.noreply.github.com>
2025-03-11 12:36:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from auth import authenticate_user, ROLE_ACCESS
-from db import get_branches
+from db import get_db_connection
 
 # Authenticate the user
 if not authenticate_user():
