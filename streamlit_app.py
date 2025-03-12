8e0c72460abac7c3fa7b90e67d65444eaddccede
minammahrous <126823445+minammahrous@users.noreply.github.com>
2025-03-12 15:52:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 from auth import authenticate_user, ROLE_ACCESS
 from db import get_branches
-
+# Hide Streamlit's menu and "Manage app" button
+st.markdown("""
+    <style>
+        [data-testid="stToolbar"] {visibility: hidden !important;}
+        [data-testid="manage-app-button"] {display: none !important;}
+        header {visibility: hidden !important;}
+        footer {visibility: hidden !important;}
+    </style>
+""", unsafe_allow_html=True)
 # Authenticate user
 user = authenticate_user()
 
